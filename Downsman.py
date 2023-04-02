f1962cdd71cbd226c6bb1863ef81e6ebd711705f
scott james <scottejames@gmail.com>
2023-04-02 16:09:59 +0100

    first go, works, need to put a tcl UI onto it

--- Downsman.py
+++ Downsman.py
@@ -1,2 +1,43 @@
-#Print hello world
-print("Hello World")
+#!/usr/local/bin/python3
+
+import openpyxl
+import os
+import sys
+import time
+
+# important places in the sheet
+CHECKPOINT_NUMBER_LOCATION = 'D4'
+CHECKPOINT_NAME_LOCATION = 'F4'
+CHECKPOINT_TIME_TARGET = 'B'
+CHECKPOINT_TIME_TARGET_ROW = 7
+
+checkpoint_number ='1'
+checkpoint_name = 'scotts checkpoint'
+checkpoint_time_target = 31
+max_points = 20
+
+path = 'Templates'
+save_path = 'Out'
+sheetName = os.path.join(path, "Template.xlsx")
+wb = openpyxl.load_workbook(sheetName)
+sheet = wb['Sheet1']
+sheet[CHECKPOINT_NUMBER_LOCATION] = checkpoint_number
+sheet[CHECKPOINT_NAME_LOCATION] = checkpoint_name
+# for loop over 20 integers
+row = CHECKPOINT_TIME_TARGET_ROW
+for i in range(0, 20):
+    row = row + 1
+    cell = CHECKPOINT_TIME_TARGET + str(row)
+    score_cell = 'C' + str(row)
+    team_cell = 'D' + str(row)
+    sheet[cell] = '00:' + str(checkpoint_time_target + i)
+    sheet[score_cell] = max_points - i
+    sheet[team_cell] = i + 1
+
+resultsName = os.path.join(save_path, "Results.xlsx")
+wb.save(resultsName)
+
+
+
+
+
